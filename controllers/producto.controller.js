ea0df52e3ee9131109416e5b478737c98f8a1407
@chavidev <javiercastaneda303@gmail.com>
2021-11-09 09:49:50 +0000

    productos.get .post .put .delete => ¿ahora pasando id?

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -18,22 +18,6 @@ const create = async (req, res) => {
     }
 }
 
-// update   con put desde postman y set desde mongoose
-const update = async (req, res) => {
-    try {
-        //const producto = req.body; // objeto que viene desde el front 
-        let artículo = await Producto.find({"id_producto":1026}) 
-        if(!id_registro)  id_registro = await ID_registro.create({ id_variacion: 1000});
-        producto.id_producto = id_registro.id_variacion
-        id_registro.id_variacion += 1
-        id_registro.save()
-        const productoCreado = await Producto.create(producto); // se crea el producto, mediante el metodo Producto.create()
-        res.status(200).json({ message: 'success', producto:productoCreado  }); // se le envia al front el producto creado y un menasaje de exitoso
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Error al crear producto...' }); 
-    }
-}
 // lectura de todos los producto
 const read = async (req, res) => {
     try {
@@ -45,7 +29,46 @@ const read = async (req, res) => {
     }
 }
 
+
+// update   con put desde postman y set desde mongoose
+const update = async (req, res) => {
+    try {
+        //const producto = req.body; // objeto que viene desde el front 
+        let modificacion = req.body
+        let producto = await Producto.findOne({"id_producto":modificacion.id_producto})  //&& se puede evitar ésta línea
+        // let response =  await Producto.updateOne({_id:producto._id},modificacion) //otra forma de hacerlo, pero no devuelve nada INTERESANTE
+        let response =  await Producto.findByIdAndUpdate(producto._id,modificacion, {new: true}) // por default false, así nos da el valor nuevo
+        //producto = {...producto,...modificacion}  //cargo todos los atributos y después modifico lo que quiera
+        //let response = await producto.save()  //Ups algo salió mal en la operación
+        res.status(200).json(response); // se le envia al front el producto creado y un menasaje de exitoso
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error al crear producto...' }); 
+    }
+}
+
+
+// delete
+const deleteProducto = async (req, res) => {
+    try {
+        //const producto = req.body; // objeto que viene desde el front 
+        let eliminar = req.body
+        let producto = await Producto.findOne({"id_producto":eliminar.id_producto})  
+        let response =  await Producto.findByIdAndDelete(producto._id)         
+        res.status(200).json(response); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error al eliminar el  producto...' }); 
+    }
+}
+
+//limpieza  clean
+//Producto.remove()
+
+
 module.exports = {
     create,
     read,
+    update,
+    deleteProducto,
 };
--- routes/routeProductos.js
+++ routes/routeProductos.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const producto = require('../controllers/producto.controller');
 
 //router.get('/productos', producto.getAll);
-router.route('/producto')
+router.route('/')
 .post( producto.create) // llama a la funcion create, que esta en controllers/producto.controller
-.get( producto.read);
-//.get( producto.gett)
-//router.route('/cliente/:id')
+.get( producto.read)
 
+/* 
+router.route('/:id')
+.get(producto.readOne)  //&&crear y actualizar
+.put( producto.update)
+.delete(producto.deleteProducto)
+ */
 
-//router.get('/producto', producto.gett);
+//router.get('/producto', producto.gett); //otro sistema, pero éste es mas limpio
 
 
 module.exports = router;

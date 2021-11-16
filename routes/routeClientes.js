080b832174c80aec0b494c8e5514ded41cd7c6a7
@chavidev <javiercastaneda303@gmail.com>
2021-11-16 13:44:49 +0000

    _

--- routes/routeClientes.js
+++ routes/routeClientes.js
@@ -6,16 +6,26 @@ const { checkToken } = require('../middlewaree/autenticarCliente.js')
 //zona de peligro limpieza de todos los clientes
 router.delete('/removed/user/:user/pass/:pass', cliente.removedAllCliente);
 
-//router.get('/:id', checkToken, cliente.readOne)
+//router.get('/:id', checkToken, cliente.readOne)  //sistema estandard del token
+
+//router.route(checkAdmin , '/admin')   // como admin no tengo limitaciones   
+router.route('/admin')  
+.post(cliente.create)
+.get(cliente.read)  
+
+router.route('admin/:id')
+.get(cliente.readOne)
+.put(cliente.update)
+.delete(cliente.deleteCliente) 
 
 router.route('/')
 .post(cliente.create)
 .get(cliente.read)
 
 router.route('/:id')
-//.get(cliente.readOne)
-.get(checkToken , cliente.readOne)
+//.get(cliente.readOne)  
+.get(checkToken , cliente.readOne)  //con token cliente
 .put(cliente.update)
 .delete(cliente.deleteCliente)
 
-module.exports = router; //&&  ¿no tendría qeu ser routerProducto?  ¿dónde lo importo?
+module.exports = router; 

4d93e303c10a1fe4267f9abc68ce927762f0f3da
@chavidev <javiercastaneda303@gmail.com>
2021-11-28 20:12:52 +0000

    solucionado los cors para poder conectar desde react3000  en Node5001

--- controllers/autenticacion.controller.js
+++ controllers/autenticacion.controller.js
@@ -1,28 +1,29 @@
-const  Cliente = require('../models/Cliente') 
+const Cliente = require('../models/Cliente')
 const ID_registro = require('../models/ID_registro')
-const jwt = require("jsonwebtoken");
-
+const jwt = require('jsonwebtoken')
 
 const loginCliente = async (req, res) => {
   try {
-        const { email, pass } = req.body
-        const cliente = await Cliente.findOne({ email })
-        if(cliente) {
-            if(cliente.pass === pass) {
-                const token = jwt.sign({ id: cliente._id },"HOLA" , { expiresIn: "10h" });
-                res.status(200).json({ message: 'Autenticado', token: token });
-            } else {
-                res.status(404).json({ message: 'Datos incorrectos' });
-            }
-        } else {
-            res.status(404).json({ message: 'Datos incorrectos' });
-        }  
+    const { email, pass } = req.body
+    console.log(req)
+    console.log(email, pass)
+    const cliente = await Cliente.findOne({ email })
+    if (cliente) {
+      if (cliente.pass === pass) {
+        const token = jwt.sign({ id: cliente._id }, 'HOLA', { expiresIn: '10h' })
+        res.status(200).json({ message: 'Autenticado', token: token })
+      } else {
+        res.status(404).json({ message: 'Datos incorrectos' })
+      }
+    } else {
+      res.status(404).json({ message: 'Datos incorrectos' })
+    }
   } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Error autenticando cliente' });
+    console.log(error)
+    res.status(500).json({ message: 'Error autenticando cliente' })
   }
 }
 
 module.exports = {
-    loginCliente
-};
+  loginCliente
+}
--- server.js
+++ server.js
@@ -1,32 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const productosRoute = require('./routes/routeProductos.js');
-const carritoRoute = require('./routes/routeCarrito.js');
-const clientesRoute = require('./routes/routeClientes.js');
-const loginClienteRoute = require('./routes/routeLoginCliente.js');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors') // cors + concurrently permite conectar el backend y el frontEnd
+const productosRoute = require('./routes/routeProductos.js')
+const carritoRoute = require('./routes/routeCarrito.js')
+const clientesRoute = require('./routes/routeClientes.js')
+const loginClienteRoute = require('./routes/routeLoginCliente.js')
 //&&clientesroute
 
-const dotenv = require('dotenv');  // utilizo las variables de ambiente
-dotenv.config({ path: './config.env' });
+const dotenv = require('dotenv') // utilizo las variables de ambiente
+dotenv.config({ path: './config.env' })
 
-const app = express();
-const port = process.env.PORT || 8080;   // si no hay puerto asignado, va al 8080
+const app = express()
+const port = process.env.PORT || 8080 // si no hay puerto asignado, va al 8080
 
-// conexion a la base de datos 
+// conexion a la base de datos
 // ups, ha de salir de las variables de ambiente
 const uri = `mongodb+srv://admin:${process.env.PASSWORD_DB}@cluster0.mzbdm.mongodb.net/caprichosa?retryWrites=true&w=majority`
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(()=> console.log('conectado a mongodb desde clientes'))
-  .catch(e => console.log('error de conexión', e)); 
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('conectado a mongodb desde clientes'))
+  .catch(e => console.log('error de conexión', e))
 
-app.use(express.urlencoded({ extended: false }));  //&&¿qué hago aquí?
-app.use(express.json());
+const whitelist = [
+  // Allow domains here
+  'http://localhost:3000/',
+  'http://localhost:3001/',
+  'http://localhost/',
+  'http://127.0.0.1:3001/'
+]
+const corsOptions = {
+  origin(origin, callback) {
+    const originIsWhitelisted = whitelist.indexOf(origin) !== -1
+    callback(null, originIsWhitelisted)
+  },
+  credentials: true
+}
+//aplicanco el cors de forma global
+app.use(cors()) // sin cors option funciona, aunque se podrá meter todo el mundo whitelist de adorno y corsOption también
+
+app.use(express.urlencoded({ extended: false })) //&&¿qué hago aquí?
+app.use(express.json())
 
 // routes api
-app.use('/api/carrito', carritoRoute);
-app.use('/api/producto', productosRoute); 
-app.use('/api/cliente', clientesRoute); 
-app.use('/api/loginCliente', loginClienteRoute);
+app.use('/api/carrito', carritoRoute)
+app.use('/api/producto', productosRoute)
+app.use('/api/cliente', clientesRoute)
+app.use('/api/loginCliente', loginClienteRoute)
 
-app.listen(port, () => { console.log('server on port:', port) });
-
+app.listen(port, () => {
+  console.log('server on port:', port)
+})
